a208715d0fb87ccb80d855e6e28cd8778dc4350f
Jezebel1990 <75287031+Jezebel1990@users.noreply.github.com>
2021-04-15 21:01:38 -0300

    Polimorfismo alterado

--- Exercicio/AnimalPreguica.java
+++ Exercicio/AnimalPreguica.java
@@ -1,19 +1,24 @@
 package Exercicio;
 
 public class AnimalPreguica  extends Animal{
-
-	private String espreguica;
-	public AnimalPreguica()
+public AnimalPreguica()
 	{
-			}
-			public String getEspreguica() {
-				return getEspreguica();
-			}
-		   public void setEspreguica() {
-			this.espreguica = espreguica;
-		   }
-		   public String getNome()
-		   {
-			   return "Nome do animal: "+super.getNome()+" - Som: "+this.getEspreguica();
-		   }
+		super("Preguica");
 	}
+        @Override
+	public void chama(int num)
+	{
+	    for(int i=0;i<num;i++)
+	{
+	
+	System.out.println("Uah ah a, a a Uah ah a, ai que preguiça!");
+	}
+}
+	@Override
+	public void emite(String som)
+	{
+
+			System.out.println("\nEste animal deve subir em árvores...  "+som);
+		}
+	}	
+	
